ea81e5f1f1ad7b25b86a0d8148572a6d14abe43c
shubhanshu <shubhanshumohanty@gmail.com>
2025-07-16 11:41:52 +0530

    move to .net 9 and setup identity

--- SmServiceCommerce/Program.cs
+++ SmServiceCommerce/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using SmServiceCommerce.DataAccess.Data;
 using SmServiceCommerce.DataAccess.Repository;
 using SmServiceCommerce.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,9 @@ builder.Services.AddControllersWithViews();
 //DbConnection
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddRazorPages();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
@@ -28,7 +32,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
-
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
