ccf8367ceb1a2d0ded1d2487149490944e160787
shubhanshu <shubhanshumohanty@gmail.com>
2025-07-23 18:12:57 +0530

    service customer controller

---
+++ SmServiceCommerce/Areas/Customer/Controllers/ServiceController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmServiceCommerce.DataAccess.Repository.IRepository;
+using SmServiceCommerce.Models;
+
+namespace SmServiceCommerce.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class ServiceController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ServiceController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SearchResult(string service, string location)
+        {
+            //List<Service> serviceList=_unitOfWork.;
+            return View(service);
+        }
+        
+    }
+}
