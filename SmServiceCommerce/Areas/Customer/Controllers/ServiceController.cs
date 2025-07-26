59989ab7144a40a216024435d613fc99d853f797
shubhanshu <shubhanshumohanty@gmail.com>
2025-07-26 21:48:11 +0530

    add custumer service controller and booking functionality

--- SmServiceCommerce/Areas/Customer/Controllers/ServiceController.cs
+++ SmServiceCommerce/Areas/Customer/Controllers/ServiceController.cs
@@ -2,6 +2,8 @@
 using Microsoft.EntityFrameworkCore;
 using SmServiceCommerce.DataAccess.Repository.IRepository;
 using SmServiceCommerce.Models;
+using SmServiceCommerce.Models.ViewModels;
+using System.Security.Claims;
 
 namespace SmServiceCommerce.Areas.Customer.Controllers
 {
@@ -19,9 +21,43 @@ namespace SmServiceCommerce.Areas.Customer.Controllers
         }
         public IActionResult SearchResult(string service, string location)
         {
-            //List<Service> serviceList=_unitOfWork.;
-            return View(service);
+            List<ApplicationUser> serviceProviderList = _unitOfWork.ApplicationUser.GetAll(includeProperties: "Service").Where(i => i.Service?.ServiceName.ToLower() == service.ToLower() && i.City?.ToLower() == location.ToLower()).ToList();
+            return View(serviceProviderList);
         }
-        
+        public IActionResult BookServiceDetail(string id)
+        {
+            ServiceProviderInfo serviceProviderInfo = _unitOfWork.ServiceProviderInfo.Get(u => u.ApplicationUserId == id, includeProperties: "User,User.Service");
+            if (serviceProviderInfo == null)
+            {
+                return NotFound();
+            }          
+            return View(serviceProviderInfo);
+        }
+
+        [HttpPost]
+        public IActionResult BookService(BookingVM bookingVM)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            var date = DateOnly.Parse(bookingVM.BookingDate);
+            var time = TimeOnly.Parse(bookingVM.BookingTime);
+            Booking booking = new Booking
+            {
+                ApplicationUserId = userId,
+                ServiceProviderId = bookingVM.ServiceProviderId,
+                BookingDate = date,
+                BookingTime = time,
+                Status = "Pending"
+            };
+            if (!string.IsNullOrEmpty(bookingVM.Notes))
+            {
+                booking.Notes = bookingVM.Notes;
+            }
+            _unitOfWork.Booking.Add(booking);
+            _unitOfWork.Save();
+            return RedirectToAction("Index", "Home");
+
+        }
     }
 }
